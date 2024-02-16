1d710503f5cd442e4ceb174ca137e01869c3dcc2
Artsiom Chekh <qbartem@gmail.com>
2024-02-16 15:52:26 +0300

    Initial commit

---
+++ CardWorkout/CardSelectionVC.swift
@@ -0,0 +1,15 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout
+//
+//  Created by Artsiom Chekh on 16.02.24.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
